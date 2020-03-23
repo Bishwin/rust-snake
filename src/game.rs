2ed88d95a557b3a9d919cf013e883bbb129ca045
Jake <jake.huggins@solirius.com>
2020-03-23 16:16:31 +0000

    Add game struct

--- src/game.rs
+++ src/game.rs
@@ -11,3 +11,20 @@ const FOOD_COLOR: Color = [0.80, 0.00, 0.00, 1.0];
 const BORDER_COLOR: Color = [0.00, 0.00, 0.00, 1.0];
 const GAMEOVER_COLOR: Color = [0.90, 0.00, 0.00, 0.5];
 
+const MOVING_PERIOD: f64 = 0.1;
+const RESTART_TIME: f64 = 1.0;
+
+
+pub struct Game {
+    snake: Snake,
+
+    food_exisits: bool,
+    food_x: i32,
+    food_y: i32,
+
+    width: i32,
+    height: i32,
+
+    game_over: bool,
+    waiting_time: f64,
+}
