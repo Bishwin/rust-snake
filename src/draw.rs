ecde612e3e467692ff8950c634020269d1cde2ab
Jake <jake.huggins@solirius.com>
2020-03-23 16:11:51 +0000

    remove whitespace

--- src/draw.rs
+++ src/draw.rs
@@ -19,7 +19,7 @@ pub fn draw_block(color: Color, x: i32, y: i32, con: &Context, g: &mut G2d) {
     );
 }
 
-pub fn draw_rectangle (
+pub fn draw_rectangle(
     color: Color,
     x: i32,
     y: i32,
