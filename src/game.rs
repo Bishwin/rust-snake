dfa9c83f06114fc819fdecf640a098925861487e
Jake <jake.huggins@solirius.com>
2020-03-23 16:14:27 +0000

    Add gameboard colours

--- src/game.rs
+++ src/game.rs
@@ -6,3 +6,8 @@ use rand::{thread_rng, Rng};
 use snake::{Direction, Snake};
 use draw::{draw_block, draw_rectangle};
 
+
+const FOOD_COLOR: Color = [0.80, 0.00, 0.00, 1.0];
+const BORDER_COLOR: Color = [0.00, 0.00, 0.00, 1.0];
+const GAMEOVER_COLOR: Color = [0.90, 0.00, 0.00, 0.5];
+
